c2dc8e8c76021f66ac1b7d9d194515a9c75b7273
Jalil <jalilsan@gmail.com>
2017-12-04 03:50:37 -0800

    Created  a separate file for Navigation

--- App.js
+++ App.js
@@ -1,73 +1,18 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import DeckList from './src/components/DeckList'
-import Deck from './src/components/Deck'
-import NewDeck from './src/components/NewDeck'
-import AddCard from './src/components/AddCard'
-import Quiz from './src/components/Quiz'
+import { RootNavigator } from './src/common/routes'
 import { Header, Card, List, ListItem, Button } from 'react-native-elements'
 import reducer from './src/reducers'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import { TabNavigator, StackNavigator } from 'react-navigation'
 import { setLocalNotification } from "./utils/notifications";
-
-const Tabs = TabNavigator({
-  DeckList: {
-    screen: DeckList,
-    navigationOptions: {
-      title: 'Deck List',
-      tabBarLabel: 'Deck List'
-    }
-  },
-  NewDeck: {
-    screen: NewDeck,
-    navigationOptions: {
-      title: 'New Deck',
-      tabBarLabel: 'New Deck'
-    }
-  }
-})
-
-const RootNavigator = StackNavigator({
-  Home: {
-    screen: Tabs
-  },
-  DeckList: {
-    screen: DeckList
-  },
-  Deck: {
-    screen: Deck,
-    navigationOptions: {
-      title: 'Deck'
-    }
-  },
-  AddCard: {
-    screen: AddCard,
-    navigationOptions: {
-      title: 'Add New Card'
-    }
-  },
-  Quiz: {
-    screen: Quiz,
-    navigationOptions: {
-      title: 'Quiz'
-    }
-  }
-})
 
 export default class App extends React.Component {
 
  componentDidMount() {
     setLocalNotification();
   }
-  renderDeck (deck) {
-    return (
-      <Text>
-        {' '}{deck.text}{' '}
-      </Text>
-    )
-  }
+
   render () {
     return (
       <Provider store={createStore(reducer)}>
---
+++ src/common/routes.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import DeckList from './../components/DeckList'
+import Deck from './../components/Deck'
+import NewDeck from './../components/NewDeck'
+import AddCard from './../components/AddCard'
+import Quiz from './../components/Quiz'
+import { TabNavigator, StackNavigator } from 'react-navigation'
+
+const Tabs = TabNavigator({
+  DeckList: {
+    screen: DeckList,
+    navigationOptions: {
+      title: 'Deck List',
+      tabBarLabel: 'Deck List'
+    }
+  },
+  NewDeck: {
+    screen: NewDeck,
+    navigationOptions: {
+      title: 'New Deck',
+      tabBarLabel: 'New Deck'
+    }
+  }
+})
+
+export const RootNavigator = StackNavigator({
+  Home: {
+    screen: Tabs
+  },
+  DeckList: {
+    screen: DeckList
+  },
+  Deck: {
+    screen: Deck,
+    navigationOptions: {
+      title: 'Deck'
+    }
+  },
+  AddCard: {
+    screen: AddCard,
+    navigationOptions: {
+      title: 'Add New Card'
+    }
+  },
+  Quiz: {
+    screen: Quiz,
+    navigationOptions: {
+      title: 'Quiz'
+    }
+  }
+})
+
+
